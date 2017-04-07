fc6e053102768b344dced5087d846862d33f9047
JohnathanLP <johnathanlpowell@gmail.com>
2017-04-07 02:23:19 -0600

    added animation registration

--- Johnathan/Rev2/scripts/gameplay.js
+++ Johnathan/Rev2/scripts/gameplay.js
@@ -1,14 +1,95 @@
 let myGame = (function(){
   //Primary Variables:
   var lastTimeStamp;
-  var girlX = 32;
+  var offset = 0;
+  var speed = 15;
+  var snap_flag = true;
+  var snapped_flag = false;
 
-  //Tile Creation
-  let girl = Graphics.Tile({
+  //Tile and Sprite Creation
+  let girl = Graphics.Sprite({
     imageSource: 'images/desert_girl.png',
-    position: {x:32, y:97},
+    position: {x:32, y:96},
     clip: {x:0, y:0, w:32, h:32}
   });
+  girl.addAnimation({
+    name: 'run',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [1,1,1],
+    delay: [1600,1600,1600]
+  });
+  girl.addAnimation({
+    name: 'breathe',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [0,0,0],
+    delay: [300,300,300]
+  });
+  girl.setAnimation('run');
+
+  let background = Graphics.Tile({
+    imageSource: 'images/desert_background.png',
+    position: {x:0, y:0},
+    clip: {x:0, y:0, w:512, h:128}
+  });
+
+  let sandTiles = [];
+  for(var i=0; i<9; i++){
+    let sel = Math.floor(Math.random()*9);
+    if(sel >= 3){
+      sel = 0;
+    }
+    let sandTile = Graphics.Sprite({
+      imageSource: 'images/sand_tiles.png',
+      position: {x:32, y:97},
+      clip: {x:32*sel, y:0, w:32, h:32}
+    });
+    sandTiles.push(sandTile);
+  }
+
+  let sand_tile = Graphics.Tile({
+    imageSource: 'images/sand_tiles.png',
+    position: {x:0, y:0},
+    clip: {x:0, y:0, w:32, h:32}
+  });
+
+  //Secondary Functions:
+  function drawSand(){
+    for(var i=0; i<sandTiles.length; i++){
+      sandTiles[i].drawAbs((i*32)-(offset%32),128);
+    }
+  }
+
+  function snapSandTiles(){
+    if(offset%32 > 18){
+      snap_flag = true;
+    }
+    if(offset%32 < 18 && snap_flag){
+      snapped_flag = false;
+    }
+    if(snap_flag && !snapped_flag){
+      //console.log('snap');
+      let sel = Math.floor(Math.random()*9);
+      if(sel >= 3){
+        sel = 0;
+      }
+      let sandTile = Graphics.Sprite({
+        imageSource: 'images/sand_tiles.png',
+        position: {x:32, y:97},
+        clip: {x:32*sel, y:0, w:32, h:32}
+      });
+      sandTiles.push(sandTile);
+      sandTiles.splice(0,1);
+      snapped_flag = true;
+      snap_flag = false;
+    }
+  }
+
+  function drawBackground(){
+    background.drawAbs(-offset/2,0);
+    background.drawAbs((-offset+512)/2,0);
+  }
 
   //Primary Functions:
   function initialize(){
@@ -24,16 +105,30 @@ let myGame = (function(){
     render();
     requestAnimationFrame(gameLoop);
   }
-  
-  function update(elapsedTime){
 
+  function update(elapsedTime){
+    var time = 0;
+    if(!isNaN(elapsedTime)){
+      time = elapsedTime;
+    }
+    girl.animate(time, speed);
+
+    offset += speed/time;
+
+    if(offset >= 512){
+      offset = 0;
+    }
+    snapSandTiles();
   }
 
   function render(){
-    girl.drawAbs(50,50);
+    drawBackground();
+    girl.drawCurr();
+    drawSand();
   }
 
   return{
-    initialize: initialize
+    initialize: initialize,
+    girl: girl
   };
 }());
--- Johnathan/Rev2/scripts/graphics.js
+++ Johnathan/Rev2/scripts/graphics.js
@@ -16,11 +16,16 @@ let Graphics = (function(){
     context.clear();
   }
 
+  //Does not have animations
   function Tile(spec){
     var that = {};
     var ready = false;
     var image = new Image();
     var animations = [];
+    var currentAnimation;
+    var counter = 0;
+    var frame = 0;
+    var animationSpeed = 1;
 
     image.onload = function(){
       ready = true;
@@ -57,14 +62,84 @@ let Graphics = (function(){
       spec.clip.y = frame.y*32;
     }
 
-    that.createAnimation = function(spec){
-      var name = spec.name;
-      var temp = []
-      var frames = spec.frames;
-      for(var iter=0; iter<frames; iter++){
-        temp[iter] = {t: spec.iter.t, x:spec.iter.x, spec.iter.y};
+    return that;
+  }
+
+  //Has animations
+  function Sprite(spec){
+    var that = {};
+    var ready = false;
+    var image = new Image();
+    var animations = [];
+    var currentAnimation;
+    var counter = 0;
+    var frame = 0;
+    var animationSpeed = 1;
+
+    image.onload = function(){
+      ready = true;
+    };
+    image.src = spec.imageSource;
+
+    that.drawAbs = function(xLoc, yLoc){
+      if(ready){
+        context.drawImage(
+          image,
+          spec.clip.x, spec.clip.y,
+          spec.clip.w, spec.clip.h,
+          xLoc, yLoc,
+          spec.clip.w, spec.clip.h);
+        context.restore();
       }
-      animations[name] = temp;
+    }
+
+    that.drawCurr = function(){
+      if(ready){
+        context.save();
+        context.drawImage(
+          image,
+          spec.clip.x, spec.clip.y,
+          spec.clip.w, spec.clip.h,
+          spec.position.x,spec.position.y,
+          spec.clip.w, spec.clip.h);
+        context.restore();
+      }
+    }
+
+    that.setFrame = function(frame){
+      spec.clip.x = frame.x*32;
+      spec.clip.y = frame.y*32;
+    }
+
+    that.addAnimation = function(spec){
+      //TODO - filter and check that animations are valid
+      animations[spec.name] = spec;
+      console.log('Animation Registered');
+    }
+
+    //IMPORTANT! You have to set an animation! If you don't set at least one, it'll crash
+    that.setAnimation = function(spec){
+      currentAnimation = spec;
+      counter = 0;
+    }
+
+    that.animate = function(elapsedTime, speed){
+      counter += (elapsedTime*animationSpeed);
+      //console.log(counter);
+      if(counter > animations[currentAnimation].delay[frame]/speed){
+        counter = 0;
+        frame++;
+        if(frame >= animations[currentAnimation].frames){
+          frame = 0;
+        }
+      }
+      that.setFrame({x:animations[currentAnimation].frameX[frame], y:animations[currentAnimation].frameY[frame]});
+    }
+
+    that.setAnimationSpeed = function(spec){
+      if(spec > 0 && spec <= 10){
+        animationSpeed = spec;
+      }
     }
 
     return that;
@@ -73,6 +148,7 @@ let Graphics = (function(){
   return{
     initialize: initialize,
     beginRender: beginRender,
-    Tile: Tile
+    Tile: Tile,
+    Sprite: Sprite
   };
 }());
