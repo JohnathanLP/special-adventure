7c6f02a8f38af76dbb41d150a06d1970bc9d9dc4
JohnathanLP <johnathanlpowell@gmail.com>
2017-04-08 01:49:27 -0600

    added rev2 code to Haleys setup

--- Johnathan/scripts/gameplay.js
+++ Johnathan/scripts/gameplay.js
@@ -1,32 +1,42 @@
 let myGame = (function(){
-  let background = Graphics.Texture({
-    imageSource: 'images/desert_background.png',
-    center: {x:0, y:0},
-    clip: {x:0, y:0, w:512, h:128},
-    width: 512,
-    height: 128,
-    rotation: 0
-  });
+  //Primary Variables:
+  var lastTimeStamp;
+  var speed = 20;
+  var offset = 0;
+  var background_offset = 0;
 
-  let sun = Graphics.Texture({
-    imageSource: 'images/sun.png',
-    center: {x:0, y:0},
-    clip: {x:0, y:0, w:19, h:19},
-    width: 20,
-    height: 20,
-    rotation: 0
-  });
-
+  //Tile and Sprite Creation
   let girl = Graphics.Sprite({
     imageSource: 'images/desert_girl.png',
-    position: {x:32, y:97},
+    position: {x:32, y:96},
     clip: {x:0, y:0, w:32, h:32}
+  });
+  girl.addAnimation({
+    name: 'run',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [1,1,1],
+    delay: [1700,1700,1700]
+  });
+  girl.addAnimation({
+    name: 'breathe',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [0,0,0],
+    delay: [8500,2500,8500]
+  });
+  girl.setAnimation('run');
+
+  let background = Graphics.Tile({
+    imageSource: 'images/desert_background.png',
+    position: {x:0, y:0},
+    clip: {x:0, y:0, w:512, h:128}
   });
 
   let sandTiles = [];
   for(var i=0; i<9; i++){
-    let sel = Math.floor(Math.random()*9);
-    if(sel >= 3){
+    let sel = Math.floor(Math.random()*15);
+    if(sel >= 4){
       sel = 0;
     }
     let sandTile = Graphics.Sprite({
@@ -37,32 +47,19 @@ let myGame = (function(){
     sandTiles.push(sandTile);
   }
 
-  var background_offset = 0;
-  var offset = 0;
-  var speed = 15;
+  let sandstorm = Graphics.ParticleSystem();
 
-  var particles = [];
-  var lastTimeStamp = performance.now();
+  //Secondary Functions:
+  function drawSand(){
+    for(var i=0; i<sandTiles.length; i++){
+      sandTiles[i].drawAbs((i*32)-(offset%32),128);
+    }
+  }
 
-  function update(elapsedTime){
-    girl.animate(elapsedTime, speed);
-    console.log(elapsedTime);
-    //background_offset -= speed;
-
-    if(!isNaN(elapsedTime)){
-      background_offset -= (speed)/elapsedTime;
-    }
-    if(background_offset < 0){
-      background_offset = 512;
-    }
-    if(!isNaN(elapsedTime)){
-      offset += (speed*2)/elapsedTime;
-    }
-    //console.log(offset);
-    if(offset >= 32){
-      offset = 0;
-      let sel = Math.floor(Math.random()*9);
-      if(sel >= 3){
+  function snapSandTiles(){
+    if(offset > 32){
+      let sel = Math.floor(Math.random()*15);
+      if(sel >= 4){
         sel = 0;
       }
       let sandTile = Graphics.Sprite({
@@ -72,83 +69,69 @@ let myGame = (function(){
       });
       sandTiles.push(sandTile);
       sandTiles.splice(0,1);
-    }
-
-    //!!! BEGIN PARTICLES: MUST BE MOVED TO GRAPHICS !!!
-    var particle = 0,
-			aliveParticles = [],
-			p;
-
-		//
-		// Go through and update each of the currently alive particles
-		aliveParticles.length = 0;
-		for (particle = 0; particle < particles.length; particle++) {
-			//
-			// A return value of true indicates this particle is still alive
-			if (particles[particle].update(elapsedTime)) {
-				aliveParticles.push(particles[particle]);
-			}
-		}
-		particles = aliveParticles;
-
-		//
-		// Generate some new particles
-    for (particle = 0; particle < 5; particle++) {
-			p = {
-				position: {x: 260, y: Random.nextGaussian(30,80)},
-				direction: {x:-.5, y:.05},
-				speed: Random.nextGaussian(400, 15),		// pixels per second
-				rotation: 0,
-				lifetime: Random.nextGaussian(2, 1)		// seconds
-			};
-
-			particles.push(Graphics.Particle(p));
-		}
-    //!!! END PARTICLES !!!
-  }
-
-  function drawSand(){
-    for(var i=0; i<9; i++){
-      //sandTile.draw((32*i)-offset,128);
-      sandTiles[i].drawTile((32*i)-offset,128);
+      offset = 0
     }
   }
 
   function drawBackground(){
-    background.draw(background_offset,0);
-    background.draw(background_offset-512,0);
+    background.drawAbs(-background_offset/2,0);
+    background.drawAbs((-background_offset+512)/2,0);
   }
 
-  function render(){
-    drawBackground();
-    sun.draw(32,32);
-    girl.draw();
-
-
-    //!!! BEGIN PARTICLES - MUST BE MOVED TO GRAPHICS !!!
-    var particle;
-
-		for (particle = 0; particle < particles.length; particle++) {
-			particles[particle].draw();
-		}
-    //!!! END PARTICLES !!!
-
-    drawSand();
+  function addSandstormParticles(){
+    for(var i=0; i<5; i++){
+      sandstorm.add({
+      position: {x: 256, y: (Math.random()*192)-64},
+      direction: {x:-5, y:.5},
+      speed: speed,
+      rotation: 0,
+      lifetime: 4,
+      width: 1,
+      height: 1,
+      fill: 'rgba(0, 0, 0, 1)',
+      stroke: 'rgba(220, 200, 0, 1)'
+      });
+    }
   }
 
-  function gameLoop(time){
-    var elapsedTime = (time - lastTimeStamp);
+  //Primary Functions:
+  function initialize(){
+    Graphics.initialize();
+    gameLoop();
+  }
+
+  function gameLoop(currTime){
+    var elapsedTime = (currTime - lastTimeStamp);
 
     update(elapsedTime);
-    lastTimeStamp = time;
-
+    lastTimeStamp = currTime;
     render();
     requestAnimationFrame(gameLoop);
   }
 
-  function initialize(){
-    Graphics.initialize();
-    gameLoop();
+  function update(elapsedTime){
+    var time = 0;
+    if(!isNaN(elapsedTime)){
+      time = elapsedTime;
+    }
+    girl.animate(time, speed);
+
+    offset += speed/time;
+    background_offset += speed/time;
+    if(background_offset >= 512){
+      background_offset = 0;
+    }
+
+    snapSandTiles();
+    sandstorm.update(time);
+    addSandstormParticles();
+  }
+
+  function render(){
+    drawBackground();
+    girl.drawCurr();
+    drawSand();
+    sandstorm.draw();
   }
 
   return{
