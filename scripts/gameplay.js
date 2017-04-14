1e4961a57e4641209e47c53a3c82466908fdfa9a
JohnathanLP <johnathanlpowell@gmail.com>
2017-04-14 12:38:33 -0600

    removed collision branch

---
+++ scripts/gameplay.js
@@ -0,0 +1,309 @@
+myGame.screens['game-play'] = (function(game) {
+  //Primary Variables:
+  var lastTimeStamp;
+  var speed = 20;
+  var offset = 0;
+  var background_offset = 0;
+  var sandstorm_intensity = 1;
+  var girlPosX = 32;
+  var distance_run = 0;
+  var cancelNextRequest = false;
+  var girlVelY = 0;
+  var onGround = true;
+  var terrH = 96;
+
+  //Tile and Sprite Creation
+  let girl = Graphics.Sprite({
+    imageSource: 'images/desert_girl.png',
+    position: {x:32, y:96},
+    clip: {x:0, y:0, w:32, h:32}
+  });
+  girl.addAnimation({
+    name: 'run',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [1,1,1],
+    delay: [1700,1700,1700]
+  });
+  girl.addAnimation({
+    name: 'breathe',
+    frames: 3,
+    frameX: [0,1,2],
+    frameY: [0,0,0],
+    delay: [8500,2500,8500]
+  });
+  girl.addAnimation({
+    name: 'jump',
+    frames: 9,
+    frameX: [0,1,2, 0,1,2, 0,1,0],
+    frameY: [2,2,2, 3,3,3, 4,4,2],
+    delay: [2000,1800,1600, 700,700,700, 1000,3000,3000]
+  });
+  girl.setAnimation('run');
+
+  let background = Graphics.Tile({
+    imageSource: 'images/desert_background.png',
+    position: {x:0, y:0},
+    clip: {x:0, y:0, w:512, h:128}
+  });
+
+  let sandTiles = [];
+  for(var i=0; i<9; i++){
+    let sel = Math.floor(Math.random()*15);
+    if(sel >= 4){
+      sel = 0;
+    }
+    let sandTile = Graphics.Sprite({
+      imageSource: 'images/sand_tiles.png',
+      position: {x:32, y:97},
+      clip: {x:32*sel, y:0, w:32, h:32}
+    });
+    sandTiles.push(sandTile);
+  }
+
+  let obstacles = [];
+  for(var i=0; i<3; i++){
+    let temp = [];
+    for(var j=0; j<9; j++){
+      temp.push(null);
+    }
+    obstacles.push(temp);
+  }
+
+  let sandstorm = Graphics.ParticleSystem();
+
+  //Secondary Functions:
+  function drawSand(){
+    for(var i=0; i<sandTiles.length; i++){
+      sandTiles[i].drawAbs((i*32)-(offset%32),128);
+    }
+  }
+
+  function snapSandTiles(){
+    if(offset > 32){
+      let sel = Math.floor(Math.random()*15);
+      if(sel >= 4){
+        sel = 0;
+      }
+      let sandTile = Graphics.Sprite({
+        imageSource: 'images/sand_tiles.png',
+        position: {x:32, y:97},
+        clip: {x:32*sel, y:0, w:32, h:32}
+      });
+      sandTiles.push(sandTile);
+      sandTiles.splice(0,1);
+      offset = 0
+    }
+  }
+
+  function drawObstacles(){
+    for(var i=0; i<obstacles.length; i++){
+      for(var j=0; j<obstacles[i].length; j++){
+        if(obstacles[i][j] != null){
+          obstacles[i][j].drawAbs((j*32)-(offset%32),32+(32*i));
+        }
+      }
+    }
+  }
+
+  function snapObstacles(){
+    if(offset > 32){
+      let sel = Math.floor(Math.random()*15);
+      let obstacle = Graphics.Sprite({
+        imageSource: 'images/obstacles.png',
+        position: {x:32, y:64},
+        clip: {x:0, y:0, w:32, h:32}
+      });
+      if(sel == 1){
+        console.log('added to row 0');
+        obstacles[0].push(obstacle);
+        obstacles[1].push(null);
+        obstacles[2].push(null);
+      }
+      if(sel == 2){
+        console.log('added to row 1');
+        obstacles[0].push(null);
+        obstacles[1].push(obstacle);
+        obstacles[2].push(null);
+      }
+      if(sel == 3){
+        console.log('added to row 2');
+        obstacles[0].push(null);
+        obstacles[1].push(null);
+        obstacles[2].push(obstacle);
+      }
+      obstacles[0].splice(0,1);
+      obstacles[1].splice(0,1);
+      obstacles[2].splice(0,1);
+    }
+  }
+
+  // function addSupports(){
+  //   if(obstacles1[obstacles1.length-1] != null){
+  //     //console.log('test');
+  //     let obstacle = Graphics.Sprite({
+  //       imageSource: 'images/obstacles.png',
+  //       position: {x:32, y:64},
+  //       clip: {x:32, y:0, w:32, h:32}
+  //     });
+  //     if(obstacles0[obstacles1.length-1] == null){
+  //       obstacles0[obstacles1.length-1] = obstacle;
+  //     }
+  //   }
+  //   if(obstacles2[obstacles2.length-1] != null){
+  //     //console.log('test2');
+  //     let obstacle = Graphics.Sprite({
+  //       imageSource: 'images/obstacles.png',
+  //       position: {x:32, y:64},
+  //       clip: {x:32, y:0, w:32, h:32}
+  //     });
+  //     if(obstacles0[obstacles1.length-1] == null){
+  //       obstacles0[obstacles1.length-1] = obstacle;
+  //     }
+  //     if(obstacles1[obstacles1.length-1] == null){
+  //       obstacles1[obstacles1.length-1] = obstacle;
+  //     }
+  //   }
+  // }
+
+  function drawBackground(){
+    background.drawAbs(-background_offset/2,0);
+    background.drawAbs((-background_offset+512)/2,0);
+  }
+
+  function addSandstormParticles(){
+    for(var i=0; i<5; i++){
+      sandstorm.add({
+      position: {x: 256, y: (Math.random()*192)-64},
+      direction: {x:-5, y:.5},
+      speed: speed,
+      rotation: 0,
+      lifetime: 4,
+      width: 1,
+      height: 1,
+      fill: 'rgba(0, 0, 0, 1)',
+      stroke: 'rgba(220, 200, 0, 1)'
+      });
+    }
+  }
+
+  function processInput(elapsedTime) {
+		myKeyboard.update(elapsedTime);
+	}
+
+  function handleJump(){
+    girl.move(0,girlVelY)
+    girlVelY += 0.1;
+    if(girl.getY() >= terrH){
+      girlVelY = 0;
+      girl.setPosition(girlPosX, terrH);
+      onGround = true;
+      girl.setAnimation('run');
+    }
+  }
+
+  function jump(){
+    if(onGround){
+      girl.setAnimation('jump');
+      onGround = false;
+      girlVelY = -3;
+    }
+  }
+
+  //Primary Functions:
+  function initialize(){
+
+    document.getElementById('id-game-play-back').addEventListener(
+		'click',
+		function() {
+			cancelNextRequest = true;
+      game.showScreen('main-menu');
+		});
+
+    Graphics.initialize();
+    AudioPlayer.initialize();
+
+    //gameLoop();
+  }
+
+  function gameLoop(currTime){
+    var elapsedTime = (currTime - lastTimeStamp);
+    processInput(elapsedTime);
+
+    update(elapsedTime);
+    lastTimeStamp = currTime;
+    render();
+
+    if (!cancelNextRequest) {
+			requestAnimationFrame(gameLoop);
+		}
+  }
+
+  function update(elapsedTime){
+    var time = 0;
+    if(!isNaN(elapsedTime)){
+      time = elapsedTime;
+    }
+    girl.animate(time, speed);
+    if(!onGround){
+      handleJump();
+    }
+
+    offset += speed/time;
+    background_offset += speed/time;
+    if(background_offset >= 512){
+      background_offset = 0;
+    }
+
+    snapObstacles();
+    //addSupports();
+    snapSandTiles();
+    sandstorm.update(time);
+    addSandstormParticles();
+  }
+
+  function render(){
+    drawBackground();
+    drawObstacles();
+    girl.drawCurr();
+    drawSand();
+    sandstorm.draw();
+  }
+
+  function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+  }
+
+  function run(){
+    cancelNextRequest = false;
+    distance_run = 0;
+    //TODO Comment this back in. I just got tired of it playing.
+    AudioPlayer.playSound('audio/desert');
+
+    myKeyboard = Input.Keyboard();
+    var keyBoardControls = Persistence.getControls();
+    console.log(keyBoardControls);
+
+    if(isEmpty(keyBoardControls)){
+      Persistence.add(74);
+      myKeyboard.registerCommand(74, jump);
+      var keyBoardControls = Persistence.getControls();
+      console.log(keyBoardControls);
+    }else{
+      console.log("we have something");
+      var jumpKey = keyBoardControls.jump;
+      myKeyboard.registerCommand(jumpKey, jump);
+    }
+
+		requestAnimationFrame(gameLoop);
+  }
+
+  return{
+    initialize: initialize,
+    run: run
+  };
+}(myGame.game));
---
+++ scripts/mainmenu.js
@@ -0,0 +1,38 @@
+myGame.screens['main-menu'] = (function(game) {
+	'use strict';
+
+	function initialize() {
+		//
+		// Setup each of menu events for the screens
+		document.getElementById('id-new-game').addEventListener(
+			'click',
+			function() { game.showScreen('game-play'); });
+
+		document.getElementById('id-high-scores').addEventListener(
+			'click',
+			function() { game.showScreen('high-scores'); });
+
+		document.getElementById('id-options').addEventListener(
+			'click',
+			function() { game.showScreen('options'); });
+
+		document.getElementById('id-sound').addEventListener(
+			'click',
+			function() { game.showScreen('sound'); });
+
+		document.getElementById('id-credits').addEventListener(
+			'click',
+			function() { game.showScreen('credits'); });
+
+	}
+
+	function run() {
+		//
+		// I know this is empty, there isn't anything to do.
+	}
+
+	return {
+		initialize : initialize,
+		run : run
+	};
+}(myGame.game));
